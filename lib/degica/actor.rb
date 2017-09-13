4224f389bd4da4c3e2896a356f73890c45f5b520
Richard Ramsden <richard@rramsden.ca>
2017-09-13 16:17:26 +0900

    Define current on Degica::Actor

--- lib/degica/actor.rb
+++ lib/degica/actor.rb
@@ -10,10 +10,17 @@ module Degica
       @actions = [Action.new(location.name, location)] + location.actions
       @objects  = Objects.new
       @focus = nil
+
+      # TODO Remove global state (v2)
+      @@current = self
     end
 
     def inspect
       @location.description
+    end
+
+    def self.current
+      @@current
     end
   end
 end
