ca7cd38a2c4e268c8de62f79a44779ef46f2db65
Richard Ramsden <richard@rramsden.ca>
2017-09-15 11:20:54 +0900

    ✨ Add a better name error message

--- lib/degica/core_ext/did_you_mean.rb
+++ lib/degica/core_ext/did_you_mean.rb
@@ -2,7 +2,7 @@ module DidYouMean
   module Correctable
     prepend_features NameError
     def to_s
-      puts "Hmm '#{name}' is not in the local context."
+      puts "Hmm (#{name}) is not in the local context. Type (actions) for a list of commands.".highlight
     end
   end
 end
