c8c610ee1e88fe9e7e9b8e1bb009f6767cb51d47
Chris Salzberg <chris@dejimata.com>
2017-09-17 21:37:20 +0900

    Allow passing *args to actions

--- lib/degica/context.rb
+++ lib/degica/context.rb
@@ -9,11 +9,11 @@ module Degica
       (@actor.actions + @focus.actions).uniq(&:name)
     end
 
-    def method_missing(method)
+    def method_missing(method, *args)
       if match = @focus.actions.find { |action| action.name == method }
-        @focus.do(match.name)
+        @focus.do(match.name, *args)
       elsif match = @actor.actions.find { |action| action.name == method }
-        @actor.do(match.name)
+        @actor.do(match.name, *args)
       else
         super
       end
