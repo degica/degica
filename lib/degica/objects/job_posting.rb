0cf7fb6481d7966f0770a4181a012e01b15eb5ae
Richard Ramsden <richard@rramsden.ca>
2017-09-17 17:20:34 +0900

    Highlight "Ruby Developer" in job description

--- lib/degica/objects/job_posting.rb
+++ lib/degica/objects/job_posting.rb
@@ -5,17 +5,18 @@ module Degica
     end
 
     def describe
-      <<~DESC
+      desc = <<~DESC
 
         Open Positions
         ==============
 
-        ## Ruby Developer
+        (Ruby Developer)
 
         We are currently hiring junior and senior level Ruby developers.
         Please e-mail a resume and CV to recruit@degica.com
 
       DESC
+      desc.highlight(:red)
     end
   end
 end
